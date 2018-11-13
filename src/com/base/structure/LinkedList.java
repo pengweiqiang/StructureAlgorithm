1191f9b961214894b3716fcdca936d371e6f7d79
pengweiqiang <pengweiqiang@shopin.cn>
2018-11-13 13:36:14 +0800

    [ADD]数据结构和算法

---
+++ src/com/base/structure/LinkedList.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2012-2018, www.shopin.net
+ * FileName: LinkedList
+ * Author:   pengweiqiang
+ * Date:     2018/11/13 12:56
+ * Description: 链表
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.base.structure;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈链表〉
+ *
+ * @author pengweiqiang
+ * @create 2018/11/13
+ * @since 1.0.0
+ */
+public class LinkedList {
+
+}
